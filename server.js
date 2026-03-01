9910d8ad201b69723c85f4c1aa8fc5824c7dac01
RoskyJ <feifreyja@gmail.com>
2026-03-01 19:33:28 +0700

    Almost Complete

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helmet = require('helmet')
 const {xss}=require('express-xss-sanitizer');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp')
-//const cors=require('cors');
+const cors=require('cors');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -26,14 +26,14 @@ app.use(helmet());
 app.use(xss());
 //Rate Limiting ไม่ให้ส่งrequestเกินค่าmax
     const limiter=rateLimit({
-    windowsMs:10*60*1000, //10 mins
-    max: 3
+    windowMs:10*60*1000, //10 mins
+    max: 100000
 });
 app.use(limiter); 
 //Prevent http param pollutions ลบค่าที่ซ้ำของ query parameter เพื่อไม่ให้attacker overrideค่า
 app.use(hpp());
 //Enable CORS เช่น frontend(localhost:3000) เรียกใช้ backend API (localhost:5000) ได้
-//app.use(cors());
+app.use(cors());
 
 //Cookie parser
 app.use (cookieParser());
@@ -42,14 +42,14 @@ app.use (cookieParser());
 connectDB();
 
 //Route files
-const hospitals = require('./routes/hospitals');
+const restaurants = require('./routes/restaurants');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const reservations = require('./routes/reservations');
 
 //Mount routers
-app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/restaurants',restaurants);
 app.use('/api/v1/auth',auth);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/reservations', reservations);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
